abc050992033dbf6610fc2cf763d579c07a980f0
aliansyahh <bb457640@gmail.com>
2021-03-01 21:44:18 +0700

    membuat template phpmvc

---
+++ app/views/template/footer.php
@@ -0,0 +1,7 @@
+</body>
+<script src="<?= BASEURL; ?>/js/bootstrap.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+</html>
